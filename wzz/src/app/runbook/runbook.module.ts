8727d967df4f9d8f40f86695fcc76224470ddbab
SYu <yushihui@gmail.com>
2017-10-12 23:23:51 -0400

    add runbook main page

--- wzz/src/app/runbook/runbook.module.ts
+++ wzz/src/app/runbook/runbook.module.ts
@@ -4,12 +4,16 @@ import {RunbookRoutingModule} from './runbook-routing.module';
 import {RunbookComponent} from './runbook.component';
 import {RunbookCardComponent} from './runbook-card/runbook-card.component';
 import {RunbookDetailComponent} from './runbook-detail/runbook-detail.component';
+import {MatButtonModule, MatIconModule, MatRippleModule} from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RunbookRoutingModule
+    RunbookRoutingModule,
+    MatIconModule,
+    MatRippleModule,
+    MatButtonModule
   ],
   declarations: [
     RunbookComponent,
