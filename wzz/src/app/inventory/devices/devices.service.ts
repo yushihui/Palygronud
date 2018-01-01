30432b4ecc7b655d78c9e5486218dae4350969d7
SYu <yushihui@gmail.com>
2018-01-01 12:00:49 -0500

    add device table

---
+++ wzz/src/app/inventory/devices/devices.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class DevicesService {
+
+  constructor() {
+  }
+
+}
+
+export interface Device {
+  id: string;
+  name: string;
+  isIPAccouting: boolean;
+  isIPSLA: boolean;
+  isNetflow: boolean;
+  isSNMPConfig: boolean;
+  mainType: number;
+  subType: number;
+  mainTypeName: string;
+  mgmtIP: string;
+  mplsAsNum: string;
+  mplsUpdate: boolean;
+}
